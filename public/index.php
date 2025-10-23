a4602811e14d92bc286787236630e8c7929c4039
Alexiitax <alexiita.ull@gmail.com>
2025-10-23 16:16:30 +0000

    process.php y form fin

--- public/index.php
+++ public/index.php
@@ -8,21 +8,34 @@
 </head>
 <body>
     <h1>Generador de Eventos</h1>
-    <form action="" method="post">
+    <form action="process.php" method="POST">
         <p>
         <label for="ponentes">Números de ponentes:</label>
-        <input type="number" placeholder="1">  
+        <input type="number" placeholder="1" name="num-ponentes">  
         </p>
         <p>
             <label for="eventos">Números de eventos:</label>
-            <input type="number" placeholder="1">
+            <input type="number" placeholder="1" name="num-eventos">
         </p>
         <p>
-            <label for="fecha">Rango del evento (fecha inicio y fecha final):</label>
-            <input type="date" id="fecha-inicio" name="fecha-inicio">
-            <input type="date" id="fecha-final" name="fecha-final">
+            <p>
+                <label for="fecha-inicio">Fecha de inicio del evento:</label>
+                <input type="date" name="fecha-inicio">
+            </p>
+            <p>
+                <label for="fecha-final">Fecha de fin del evento:</label>
+                <input type="date" name="fecha-final">
+            </p>
+            <p>
+                <label for="formato">Formato del calendario:</label>
+                <select name="formato">
+                    <option value="HTML">HTML</option>
+                    <option value="PDF">PDF</option>
+                    <option value="CSV">CSV</option>
+                </select>
+            </p>
         </p>
-        <input type="submit" value="Enviar">
+        <input type="submit" value="Enviar" id="boton-enviar">
     </form>
 </body>
 </html>
